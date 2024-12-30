05842aeaf09a7e171aa07d0bab47912754fa2177
tharindu-malshan <tmalshan16@gmail.com>
2024-12-30 20:04:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,80 +1,23 @@
-const form = document.getElementById("genarator");
-const qr = document.getElementById("qrcode");
+document.getElementById("generateBtn").addEventListener("click", function () {
+  const idNumber = document.getElementById("idNumber").value;
 
-
-
-const onGenarateSubmit = (e) => {
-  e.preventDefault();
-  cleanui();
-
-
-  const url = document.getElementById("url").value;
-  const size = document.getElementById("size").value;
-  console.log(url,size);
-
-  if(url===""){
-    alert("please enter a valid URL");
-  }
-  else{
-    showspinner();
-  
-    setTimeout(() => {
-      hidespinner();
-      genarateQrCode(url,size);
-
-      setTimeout(() => {
-        const saveurl=qr.querySelector("img").src;
-        createSaveBtn(saveurl);
-      }, 50);
-    }, 1000);
+  if (!idNumber) {
+    alert("Please enter an ID number!");
+    return;
   }
 
-}
+  const qrcodeContainer = document.getElementById("qrcode");
+  qrcodeContainer.innerHTML = ""; // Clear previous QR code
 
-const genarateQrCode = function (url,size) {
-   
-   const qrcode = new QRCode("qrcode", {
-    text: url,
-    width: size,
-    height: size,
-    colorDark : "#000000",
-    colorLight : "#ffffff",
-    correctLevel : QRCode.CorrectLevel.H
+  QRCode.toCanvas(qrcodeContainer, idNumber, {
+    width: 200,
+    margin: 2,
+    color: {
+      dark: "#000000",
+      light: "#ffffff",
+    },
+  }, function (error) {
+    if (error) console.error(error);
+    console.log("QR Code generated!");
+  });
 });
-  
-}
-
- const cleanui= function(){
-  qr.innerHTML="";
-  const savebtn = document.getElementById("savelink");
-
-  if(savebtn){
-    savebtn.remove;
-  }
- }
-
-
-
-const showspinner= function(){
-   document.getElementById("spining").style.display="block";
-}
-
-const hidespinner= function(){
-  document.getElementById("spining").style.display="none";
-}
-
-const createSaveBtn = function(saveurl){
-
-const link = document.createElement("a");
-link.id ="savelink";
-link.classList ="download-btn";
-link.href=saveurl;
-link.download="qrcode";
-link.innerHTML="Save QR Code";
-document.getElementById("genarator").appendChild(link);
-
-}
-
-hidespinner();
-
-form.addEventListener("submit",onGenarateSubmit)
